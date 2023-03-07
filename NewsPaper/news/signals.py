75ed30bf56260432d5e8750098efc77075a2a4d4
IrinaKolner <115484055+IrinaKolner@users.noreply.github.com>
2023-03-07 13:28:43 +0300

    Update signals.py

--- NewsPaper/news/signals.py
+++ NewsPaper/news/signals.py
@@ -8,44 +8,39 @@ from django.conf import settings
 
 
 
-#
-# def send_notifications(preview, pk, title, subscribers):
-#     html_content = render_to_string(
-#         'post_created_email.html',
-#         {
-#             'text': preview,
-#             'link': f'{settings.SITE_URL}/posts/{pk}'
-#             # 'link': f'http://127.0.0.1:8000/posts/{pk}'
-#         }
-#     )
-#
-#     msg = EmailMultiAlternatives(
-#         subject=title,
-#         body='',
-#         from_email=settings.DEFAULT_FROM_EMAIL,
-#         # from_email='ann.annannanna@yandex.ru',
-#         to=subscribers,
-#     )
-#
-#     msg.attach_alternative(html_content, 'text/html')
-#     msg.send()
-#
-#
-# # первый аргумент - сигнал, т.е., когда событие должно срабатывать
-# # в данном случае - m2m_changed - он срабатывает, когда мы в статью добавляем категорию
-# # т.е. когда создаем статью и определяем ее категорию
-# # sender - та модель, которая только что была изменена
-# @receiver(m2m_changed, sender=PostCategory)
-# def notify_about_new_post(sender, instance, **kwargs):
-#     if kwargs['action'] == 'post_add':
-#         categories = instance.categories.all()
-#         subscribers: list[str] = []
-#         for category in categories:
-#             subscribers += category.subscribers.all()
-#
-#         subscribers = [s.email for s in subscribers]
-#
-#         # send_notifications - эта функция и будет отправлять сообщения (она находится выше)
-#         send_notifications(instance.preview(), instance.pk, instance.title, subscribers)
-#
+
+def send_notifications(preview, pk, title, subscribers):
+    html_content = render_to_string(
+        'post_created_email.html',
+        {
+            'text': preview,
+            'link': f'{settings.SITE_URL}/posts/{pk}'
+            # 'link': f'http://127.0.0.1:8000/posts/{pk}'
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject=title,
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        # from_email='ann.annannanna@yandex.ru',
+        to=subscribers,
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+@receiver(m2m_changed, sender=PostCategory)
+def notify_about_new_post(sender, instance, **kwargs):
+    if kwargs['action'] == 'post_add':
+        categories = instance.categories.all()
+        subscribers: list[str] = []
+        for category in categories:
+            subscribers += category.subscribers.all()
+
+        subscribers = [s.email for s in subscribers]
+
+        send_notifications(instance.preview(), instance.pk, instance.title, subscribers)
+
 
